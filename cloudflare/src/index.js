78582de844feca54b45383f2561415006cc59564
Gabriel Musial <gms@kmd.dk>
2026-06-22 13:39:24 +0200

    update schedule

--- cloudflare/src/index.js
+++ cloudflare/src/index.js
@@ -13,25 +13,49 @@
  */
 
 export default {
-	async fetch(req) {
-		const url = new URL(req.url)
-		url.pathname = "/__scheduled";
-		url.searchParams.append("cron", "* * * * *");
-		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
-	},
+	// async fetch(req) {
+	// 	const url = new URL(req.url)
+	// 	url.pathname = "/__scheduled";
+	// 	url.searchParams.append("cron", "* * * * *");
+	// 	return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
+	// },
 
-	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
-	// [[triggers]] configuration.
+	// // The scheduled handler is invoked at the interval set in our wrangler.jsonc's
+	// // [[triggers]] configuration.
+	// async scheduled(event, env, ctx) {
+	// 	// A Cron Trigger can make requests to other endpoints on the Internet,
+	// 	// publish to a Queue, query a D1 Database, and much more.
+	// 	//
+	// 	// We'll keep it simple and make an API call to a Cloudflare API:
+	// 	let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
+	// 	let wasSuccessful = resp.ok ? 'success' : 'fail';
+
+	// 	// You could store this result in KV, write to a D1 Database, or publish to a Queue.
+	// 	// In this template, we'll just log the result:
+	// 	console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+	// },
 	async scheduled(event, env, ctx) {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		const url = "https://api.github.com/repos/amonbts/stock/actions/workflows/deploy.yml/dispatches";
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
-	},
+		const res = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Authorization": `Bearer ${env.GITHUB_TOKEN}`,
+			"Accept": "application/vnd.github+json",
+			"X-GitHub-Api-Version": "2022-11-28",
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			ref: "main"
+		})
+		});
+
+		if (!res.ok) {
+		const body = await res.text();
+		console.error("GitHub dispatch failed:", res.status, body);
+		throw new Error(`Dispatch failed: ${res.status}`);
+		}
+
+		console.log("GitHub workflow dispatched");
+	}
 };
